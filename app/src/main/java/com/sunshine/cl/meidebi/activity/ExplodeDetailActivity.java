59fe744d841533e8f49366fd5f088e93c49ff0af
Administrator <1275354883@qq.com>
2016-10-26 09:19:02 +0800

    未整合项目二次提交

---
+++ app/src/main/java/com/sunshine/cl/meidebi/activity/ExplodeDetailActivity.java
@@ -0,0 +1,15 @@
+package com.sunshine.cl.meidebi.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sunshine.cl.meidebi.R;
+
+public class ExplodeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explode_detail);
+    }
+}
